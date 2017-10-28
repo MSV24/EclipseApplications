0b456282ea38d07385483819bf324479792a55ca
Miriam Serrano <Miriam Serrano@PCMIRIAM>
2017-10-28 19:20:25 +0200

    First Commit

---
+++ src/Entrega.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Entrega.cpp
+// Author      : Miriam Serrano Vallejo
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!";
+	return 0;
+}
